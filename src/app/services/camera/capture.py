6d8fa5f5058084f606915f6c60c47af4e1339762
owner <owner@localhost>
2026-04-05 23:41:19 +0900

    コードの整備

--- src/app/services/camera/capture.py
+++ src/app/services/camera/capture.py
@@ -1,16 +1,16 @@
 from PIL import Image, ImageTk # OpenCV画像 → Tkinter表示用に変換
 import cv2 # カメラ・画像処理ライブラリ
 
-def imgcap_to_imgtk(frame):
+def imgcap_to_imgtk(frame_cap):
     """
     キャプチャー画像をTkinter用画像に変換
     """
 
     # BGR → RGB（OpenCVとPILの色順の違い対応）
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_tk = cv2.cvtColor(frame_cap, cv2.COLOR_BGR2RGB)
 
     # numpy配列 → PIL画像
-    img = Image.fromarray(frame)
+    img = Image.fromarray(frame_tk)
 
     # PIL画像 → Tkinter用画像
     imgtk = ImageTk.PhotoImage(image=img)
